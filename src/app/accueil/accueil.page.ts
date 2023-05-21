51fb4901671d9f14fe245286b6e97bfd825ef5db
Karl Reype <genuisxl@gmail.fr>
2023-05-21 15:20:01 +0200

    Commit scss

--- src/app/accueil/accueil.page.ts
+++ src/app/accueil/accueil.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from "../shared/authentication-service";
-import {AngularFireAuth} from "@angular/fire/compat/auth";
+import { AuthenticationService } from '../shared/authentication-service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-accueil',
@@ -8,13 +8,28 @@ import {AngularFireAuth} from "@angular/fire/compat/auth";
   styleUrls: ['./accueil.page.scss'],
 })
 export class AccueilPage implements OnInit {
+  myImagePath = '../../assets/accueil.png';
 
-  constructor(public authService: AuthenticationService, public ngFireAuth: AngularFireAuth) { }
+  data = [
+    {
+      title: 'Défis du jour',
+    },
+    {
+      title: 'Evènements de la semaine',
+    },
+    {
+      title: 'Mes associations',
+    },
+  ];
+
+  constructor(
+    public authService: AuthenticationService,
+    public ngFireAuth: AngularFireAuth
+  ) {}
 
   ngOnInit() {
     this.ngFireAuth.currentUser.then((user: any) => {
       console.log(user);
-      });
+    });
   }
-
 }
